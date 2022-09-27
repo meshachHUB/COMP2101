cd0c16ecb388951e61339e13d6f96e58d8584a73
meshachHUB <113044233+meshachHUB@users.noreply.github.com>
2022-09-26 22:41:13 -0400

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# sysinfo.sh 
+
+hostname=$(hostname)
+
+Operatingsystem=$(hostnamectl)
+
+ipaddress=$(hostname -I )
+
+rootfilesystem=$(df -h | grep /dev/sda3 )
+
+cat <<EOF
+
+Report for $hostname
+######################################
+
+Operating System And version :$Operatingsystem
+Ip Address :$ipaddress
+Root FIlesystem Free Status:$rootfilesystem
+
+######################################
+EOF
